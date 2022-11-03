c08a7652d0e502506489ab5a955c03dfa3960e44
hani Khellafi <hanikhellafi@gmail.com>
2022-11-03 11:04:18 -0500

    bank project added

---
+++ src/test/java/com/learning/Bankapp/BankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.Bankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
